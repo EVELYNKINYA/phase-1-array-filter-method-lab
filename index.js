27e03637ae33ac99e0ab073da19aeafab3ebd527
EVELYNKINYA <evelynkinya156@gmail.com>
2023-12-20 22:34:52 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+// findMatching function
+function findMatching(drivers, name) {
+    // Use filter to find all drivers that match the passed-in name
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+// fuzzyMatch function
+function fuzzyMatch(drivers, letters) {
+    // Use filter to find drivers where the name starts with the provided letters
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  //  matchName function
+function matchName(drivers, name) {
+    // Use filter to find drivers with the provided name
+    return drivers.filter(driver => driver.name === name);
+  }
+  
